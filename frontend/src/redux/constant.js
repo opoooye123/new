43485434033fb63ec069b55c52dd4c37f7127221
opooye123 <amnotpissed@gmail.com>
2025-05-31 18:31:22 +0100

    init

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,3 +1,3 @@
 export const BASE_URL = import.meta.env.VITE_API_URL.replace(/\/$/, '');
 
-export const USERS_URL = `${BASE_URL}/users`;  // 'https://back-l31n.onrender.com/api/users'
+export const USERS_URL = '/api/users';  // 'https://back-l31n.onrender.com/api/users'
